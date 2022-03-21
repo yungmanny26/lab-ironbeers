4d1eac8aa76cc28b7739079fa961964e7e8443b1
yungmanny26 <tondreau24@gmail.com>
2022-03-21 15:04:31 -0400

    done

--- app.js
+++ app.js
@@ -14,9 +14,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/beers/:id', (req, res) => {
+  // console.log(req.params.id)
+  punkAPI
+    .getBeer(req.params.id)
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render('random-beers', { beer });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.get('/', (req, res) => {
   res.render('index');
